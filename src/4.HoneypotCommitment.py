2d5a243041b57527655a9a31f571ba0248e90ae8
rogersala21 <rogersalamimo@gmail.com>
2025-06-09 23:43:44 +0200

    added possibility to change network (testnet/mainnet), added the option to create a funding transaction with an OP_RETURN to commit data about the honeypot, added functionality that saves the honeypot address into .txt

--- src/4.HoneypotCommitment.py
+++ src/4.HoneypotCommitment.py
@@ -1,7 +1,11 @@
 import os
 from bitcoinutils.setup import setup
-from bitcoinutils.keys import PublicKey, PrivateKey
-from bitcoinutils.utils import tweak_taproot_pubkey, tweak_taproot_privkey, tagged_hash
+from bitcoinutils.keys import PublicKey
+from bitcoinutils.utils import tweak_taproot_pubkey, tagged_hash
+from bitcoinutils.utils import to_satoshis
+from bitcoinutils.transactions import Transaction, TxInput, TxOutput, TxWitnessInput
+from bitcoinutils.keys import PrivateKey
+from bitcoinutils.script import Script
 
 AGGKEY_DIR = "../outputs/coordinator/key_agg_output/aggregation_output.txt"
 
@@ -20,9 +24,69 @@ def get_agg_key(file_path):
             return f.readline().strip() or None
     return None
 
+def create_op_return_tx(network, taproot_address):
+    # always remember to setup the network
+    setup(network)
+    while True:
+        response = input("Enter the private key WIF: ")
+        try:
+            priv = PrivateKey(response)
+            print("Private key:", priv.to_wif())
+            pub = priv.get_public_key()
+            break  # Exit loop if successful
+        except ValueError as e:
+            print(f"Invalid WIF: {e}. Please try again.")
+
+
+    from_address = pub.get_taproot_address()
+    print("From address:", from_address.to_string())
+
+    txid = input("Enter the txid of your UTXO: ").strip()
+    vout = int(input("Enter the vout of your UTXO (as integer): ").strip())
+    amount_btc = float(input("Enter the amount of the input UTXO (in BTC): ").strip())
+    amounts = [to_satoshis(amount_btc)]
+
+    utxos_script_pubkeys = [from_address.to_script_pub_key()]
+
+    to_address = taproot_address
+
+    txin = TxInput(txid, vout)
+
+    plain_text = input("Enter the OP_RETURN message (plain text): ").strip()
+    op_return_script = ["OP_RETURN", plain_text.encode('utf-8').hex()]
+    op_return_script = Script(op_return_script)
+    op_return_output = TxOutput(0, op_return_script)
+
+    pay_amount_btc = float(input("Enter the payment output amount (in BTC): ").strip())
+    payment_output = TxOutput(to_satoshis(pay_amount_btc), to_address.to_script_pub_key())
+
+    tx = Transaction([txin], [op_return_output, payment_output], has_segwit=True)
+
+    sig = priv.sign_taproot_input(tx, 0, utxos_script_pubkeys, amounts)
+    tx.witnesses.append(TxWitnessInput([sig]))
+
+    if network == "testnet":
+        explorer_url = "https://mempool.space/testnet4/tx/preview#hex="
+    else:
+        explorer_url = "https://mempool.space/tx/preview#hex="
+
+    print(f"\nRaw signed transaction ready to preview and broadcast here: {explorer_url}" + tx.serialize())
+
+
 def main():
-    # Initialize Bitcoin testnet
-    setup("testnet")
+    # Initialize Bitcoin network
+    while True:
+        net_choice = input("Select network: (m)ainnet or (t)estnet?: ").strip().lower()
+        if net_choice == "t":
+            network = "testnet"
+            break
+        elif net_choice == "m":
+            network = "mainnet"
+            break
+        else:
+            print("Invalid input. Please enter 't' for testnet or 'm' for mainnet.")
+
+    setup(network)
 
     # Get commitments from both folders (aggregated pubkey, and all ecies outputs)
     commitment1 = create_commitment_from_folder('../outputs/coordinator/key_agg_output')
@@ -59,8 +123,23 @@ def main():
     taproot_address = tweaked_pubkey.get_taproot_address()
     print("Taproot address from tweaked public key:", taproot_address.to_string())
 
-    # TODO: Save the taproot address to a file
-    # TODO: OP_RETURN tx for honeypot commitment (do a dialog and give the coordinator the option to generate a tx with op_ret)
+    #Save the taproot address to a file
+    with open('../outputs/coordinator/honeypot_address.txt', 'w') as f:
+        f.write(taproot_address.to_string())
+    print("Taproot address saved to ../outputs/coordinator/honeypot_address.txt" '\n')
+
+
+    #OP_RETURN tx for funding honeypot give the coordinator the option to generate a tx with op_ret
+    while True:
+        response = input("Do you want to generate a funding transaction to the honeypot with OP_RETURN data? (yes/no): ")
+        if response.lower() == "yes":
+            create_op_return_tx(network, taproot_address)
+            break
+        elif response.lower() == "no":
+            print("Exiting...")
+            break
+        else:
+            print("Invalid input. Please enter 'yes' or 'no'.")
 
 
 
